4cc45b4fee89784b34523ce229006f3f6633e3c1
BrandonWu500 <brwu17@gmail.com>
2023-11-04 10:31:40 -0400

    feat: create testimonial section
    and add to home page

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@ import CaseStudies from '@/components/case-studies';
 import Hero from '@/components/hero';
 import LogoCloud from '@/components/logo-cloud';
 import Team from '@/components/team';
+import Testimonial from '@/components/testimonial';
+import logoPhobiaDark from '@/images/clients/phobia/logo-dark.svg';
 
 const HomePage = () => {
   return (
@@ -10,6 +12,15 @@ const HomePage = () => {
       <Team />
       <LogoCloud />
       <CaseStudies />
+      <Testimonial
+        className="mt-24 sm:mt-32 lg:mt-40"
+        client={{ name: 'Phobia', logo: logoPhobiaDark }}
+      >
+        Mike and Harvey were an absolute pleasure to work with! They took care
+        of all the legal stuff surrounding the release of our new product. We
+        really appreciated how invested they were in our business. Highly
+        recommend them!
+      </Testimonial>
     </div>
   );
 };
