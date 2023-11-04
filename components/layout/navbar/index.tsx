dcea8ab084e5d8668c7e5b77c94fd126ea53610d
BrandonWu500 <brwu17@gmail.com>
2023-11-03 22:54:28 -0400

    feat: remove border radius from navbar cta button

--- components/layout/navbar/index.tsx
+++ components/layout/navbar/index.tsx
@@ -44,7 +44,7 @@ const Navbar = () => {
         </div>
         <Link
           href="/contact-us"
-          className="rounded-md bg-neutral-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-neutral-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-neutral-600"
+          className="bg-neutral-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-neutral-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-neutral-600"
         >
           Contact Us
         </Link>
