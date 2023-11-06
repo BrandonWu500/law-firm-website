e7c696bce5361c709b35df8d3926a06a114b84b5
BrandonWu500 <brwu17@gmail.com>
2023-11-06 14:06:36 -0500

    feat: add author and post types

--- types/index.ts
+++ types/index.ts
@@ -2,3 +2,19 @@ export type Menu = {
   title: string;
   path: string;
 };
+
+export type Author = {
+  node: {
+    name: string;
+    description: string;
+  };
+};
+
+export type Post = {
+  title: string;
+  uri: string;
+  excerpt: string;
+  date: string;
+  author: Author;
+  content: string;
+};
